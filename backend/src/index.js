0bf8d99f6789105eb4baa778d98ac1e41b642d7e
arthurdb1999 <arthurdb1999@gmail.com>
2020-01-15 00:46:24 -0300

    DevRadar Init

---
+++ backend/src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose')
+const routes = require('./routes');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://arthurdb1999:arthurdb1999@researchroom-vdqcn.mongodb.net/DevRadar?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+});
+
+app.use(express.json());
+app.use(routes)
+
+app.listen(3333);
